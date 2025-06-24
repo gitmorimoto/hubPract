363326484a3b0925db241ac2eef1268343bc3e4e
takehiko morimoto <forestman@vega.ocn.ne.jp>
2025-06-25 06:01:12 +0900

    2025/6/25 0:01

--- example_0.py
+++ example_0.py
@@ -1,3 +1,12 @@
 print("Hello World!")
 print("This is a file for practicing python program.")
 print("This code is executed on 2025/6/25 5:44")
+for i in range(10):
+    print("Hello, World")
+import math
+l=4
+w=10
+d=math.sqrt(l**2+w**2)
+print("d=",d)
+print\
+("d=",d)
